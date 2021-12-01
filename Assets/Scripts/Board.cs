ba5aa9361bcb6c0d78a7a512e4c7d5fd73d01a78
DESKTOP-L3GF6BJ\Luca <lucavolpato00@gmail.com>
2021-12-01 13:31:47 +0100

    Initial commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,144 @@
+
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    public Tilemap Tilemap { get; private set; }
+    public Piece ActivePiece { get; private set; }
+    public TetrominoData[] tetrominoes;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-boardSize.x / 2, -boardSize.y / 2);
+            return new RectInt(position, boardSize);
+        }
+    }
+
+    private void Awake()
+    {
+        Tilemap = GetComponentInChildren<Tilemap>();
+        ActivePiece = GetComponent<Piece>();
+        for (int i = 0; i < tetrominoes.Length; i++)
+        {
+            tetrominoes[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, tetrominoes.Length);
+        TetrominoData data = tetrominoes[random];
+
+        Vector3Int spawnPosition = new Vector3Int()
+        {
+            x = -1,
+            y = 9 - data.cells.Max(x => x.y),
+            z = 0
+        };
+
+        ActivePiece.Initialize(this, spawnPosition, data);
+        Set(ActivePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.Cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.Cells[i] + piece.Position;
+            Tilemap.SetTile(tilePosition, piece.TetrominoData.tile);
+        }
+    }
+
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.Cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.Cells[i] + piece.Position;
+            Tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        for (int i = 0; i < piece.Cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.Cells[i] + position;
+
+            if (!Bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+            if (Tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void ClearLines()
+    {
+        RectInt bounds = Bounds;
+        int row = bounds.yMin;
+        while (row < bounds.yMax)
+        {
+            if (IsLineFull(row))
+            {
+                LineClear(row);
+            }
+            else
+            {
+                row++;
+            }
+        }
+    }
+
+    private void LineClear(int row)
+    {
+        RectInt bounds = Bounds;
+        for (int col = bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+            Tilemap.SetTile(position, null);
+        }
+
+        while (row < bounds.yMax)
+        {
+            for (int col = bounds.xMin; col < bounds.xMax; col++)
+            {
+                Vector3Int position = new Vector3Int(col, row + 1, 0);
+                TileBase above = Tilemap.GetTile(position);
+
+                position = new Vector3Int(col, row, 0);
+                Tilemap.SetTile(position, above);
+            }
+
+            row++;
+        }
+    }
+
+    private bool IsLineFull(int row)
+    {
+        RectInt bounds = Bounds;
+        for (int col = bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+            if (!Tilemap.HasTile(position))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public Board Board { get; private set; }
+    public TetrominoData TetrominoData { get; private set; }
+    public Vector3Int[] Cells { get; private set; }
+    public Vector3Int Position { get; private set; }
+    public int RotationIndex { get; private set; }
+
+    public float stepDelay = 1;
+    public float lockDelay = 0.5f;
+
+    private float stepTime;
+    private float lockTime;
+
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
+    {
+        Board = board;
+        Position = position;
+        TetrominoData = data;
+        RotationIndex = 0;
+        stepTime = Time.time + stepDelay;
+        lockTime = 0;
+
+        if (Cells == null)
+        {
+            Cells = new Vector3Int[data.cells.Length];
+        }
+
+        for (int i = 0; i < Cells.Length; i++)
+        {
+            Cells[i] = (Vector3Int)data.cells[i];
+        }
+    }
+
+    private void Update()
+    {
+        Board.Clear(this);
+
+        lockTime += Time.deltaTime;
+
+        GetInput();
+
+        if (Time.time >= stepTime)
+        {
+            Step();
+        }
+
+        Board.Set(this);
+    }
+
+    private void Step()
+    {
+        stepTime = Time.time + stepDelay;
+
+        Move(Vector2Int.down);
+
+        if (lockTime > lockDelay)
+        {
+            Lock();
+        }
+    }
+
+    private void Lock()
+    {
+        Board.Set(this);
+        Board.ClearLines();
+        Board.SpawnPiece();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(Vector2Int.down);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Rotate(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Rotate(1);
+        }
+    }
+
+    private void Rotate(int direction)
+    {
+        int originalRotation = RotationIndex;
+        RotationIndex = Wrap(RotationIndex + direction, 0, 4);
+        ApplyRotationMatrix(direction);
+        if (!TestWallKicks(RotationIndex, direction))
+        {
+            RotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    private void ApplyRotationMatrix(int direction)
+    {
+        float[] matrix = Data.RotationMatrix;
+        for (int i = 0; i < Cells.Length; i++)
+        {
+            Vector3 cell = Cells[i];
+            int x, y;
+            switch (TetrominoData.tetromino)
+            {
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * matrix[0] * direction) + (cell.y * matrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * matrix[2] * direction) + (cell.y * matrix[3] * direction));
+                    break;
+                default:
+                    x = Mathf.RoundToInt((cell.x * matrix[0] * direction) + (cell.y * matrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * matrix[2] * direction) + (cell.y * matrix[3] * direction));
+                    break;
+            }
+            Cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+    private bool TestWallKicks(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
+        for (int i = 0; i < TetrominoData.wallKicks.GetLength(1); i++)
+        {
+            Vector2Int translation = TetrominoData.wallKicks[wallKickIndex, i];
+            if (Move(translation))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = rotationIndex * 2;
+
+        if (rotationDirection < 0)
+        {
+            wallKickIndex--;
+        }
+        return Wrap(wallKickIndex, 0, TetrominoData.wallKicks.GetLength(0));
+    }
+
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+
+        Lock();
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = Position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+        bool valid = Board.IsValidPosition(this, newPosition);
+        if (valid)
+        {
+            Position = newPosition;
+            lockTime = 0f;
+        }
+
+        return valid;
+    }
+
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+        {
+            return max - (min - input) % (max - min);
+        }
+        else
+        {
+            return min + (input - min) % (max - min);
+        }
+    }
+}
